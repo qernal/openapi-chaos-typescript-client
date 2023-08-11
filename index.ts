5be2d75aa2eccf66bb3fe4aeb04ad097fecc9d9f
andrew-s <github-actions@github.com>
2023-08-11 02:33:26 +0000

    Updated client repo

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-export * from "./http/http.ts";
-export * from "./auth/auth.ts";
-export * from "./models/all.ts";
-export { createConfiguration } from "./configuration.ts"
-export type { Configuration } from "./configuration.ts"
-export * from "./apis/exception.ts";
-export * from "./servers.ts";
-export { RequiredError } from "./apis/baseapi.ts";
-
-export type { PromiseMiddleware as Middleware } from './middleware.ts';
-export { PromiseOrganisationsApi as OrganisationsApi,  PromiseProjectsApi as ProjectsApi } from './types/PromiseAPI.ts';
-
+/* tslint:disable */
+/* eslint-disable */
+export * from './runtime';
+export * from './apis';
+export * from './models';
